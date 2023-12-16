a56a8d2b88457bb65b722a3f56f9a4302fcf92be
muzy4ukV <muzychuk.vitaliy@lll.kpi.ua>
2023-12-16 23:52:49 +0300

    Updates

---
+++ precache-manifest.0bbaf9ddf3ba478c2b0a05efbe16f428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb3c7ffaf9e2eca3f2d1cba096500a6c",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "c4266873ebe56c2c81de",
+    "url": "/react-app/static/css/main.a29773ed.chunk.css"
+  },
+  {
+    "revision": "1284405d5c54c1e83ff0",
+    "url": "/react-app/static/js/2.f1196f68.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-app/static/js/2.f1196f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4266873ebe56c2c81de",
+    "url": "/react-app/static/js/main.348a04ea.chunk.js"
+  },
+  {
+    "revision": "c143a55ee95f558c327c",
+    "url": "/react-app/static/js/runtime-main.85a388e9.js"
+  }
+]);
