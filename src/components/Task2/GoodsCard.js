287d28097b4aac2b82c5018acb1a4ef22efd4696
muzy4ukV <muzychuk.vitaliy@lll.kpi.ua>
2023-12-17 00:04:53 +0300

    Fixed photos src

--- src/components/Task2/GoodsCard.js
+++ src/components/Task2/GoodsCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 function Good(props) {
     return(
         <div className='good'>
-            <img src={`/goods/img_${props.id}.jpg`} width={300} height={300} alt='препарат' />
+            <img src={`./goods/img_${props.id}.jpg`} width={300} height={300} alt='препарат' />
             <p><b>{props.name}</b></p>
             <p>Ціна - {props.price} грн</p>
         </div>
